b6544ec688f61b51d356b5aa705d43f6a2d8bb03
benmahdjoubi <benmahdjoubi@outlook.com>
2020-10-14 19:28:50 +0100

    Add bubbleSort file

---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    n = len(arr) 
+
+    for i in range(n-1): 
+    
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1] :
+                print(arr) 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr 
+    
+
+arr = [8, 4, 6, 3, 0, 9]
+print(bubbleSort(arr))
+
+
